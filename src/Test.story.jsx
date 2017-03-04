36ef61cdc12b821f6ae257317fba5d3a3b94e1ca
Victor Follet <follet.victor@gmail.com>
2017-03-04 23:12:52 +0300

    some changes

--- src/Test.story.jsx
+++ src/Test.story.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-module.exports = ({ storiesOf, action }) => {
-  return storiesOf('General/Test', module)
-    .add('default', () => {
-      return <div onClick={action('click')}>
-        The test
-      </div>
-    })
+module.exports = ({storiesOf, action}) => {
+  return storiesOf('General/Test', module).add('default', () => {
+    return <div onClick={action('click')}>
+      The test
+    </div>
+  })
 }
